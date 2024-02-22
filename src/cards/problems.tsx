64ab41faf01e1ee35421220124d649f9418854ae
Starcea <stardev.uwu@gmail.com>
2024-02-23 04:22:33 +0900

    feat(problems): add mantle

--- src/cards/problems.tsx
+++ src/cards/problems.tsx
@@ -11,7 +11,7 @@ export const problemCard = async (params: {
 }) => {
   const { user, stats, size, isDark } = params
   const sizeConv = size / 200
-  const { base } = themeMapping.get(isDark)!
+  const { base, mantle } = themeMapping.get(isDark)!
 
   const pie = d3
     .pie<ProblemStat>()
@@ -32,54 +32,64 @@ export const problemCard = async (params: {
         fontSize: 28 * sizeConv,
       }}
     >
-      <svg
+      <div
         style={{
-          width: 160 * sizeConv,
-          height: 160 * sizeConv,
-          margin: 20 * sizeConv,
+          display: 'flex',
+          width: 170 * sizeConv,
+          height: 170 * sizeConv,
+          margin: 15 * sizeConv,
+          backgroundColor: mantle,
+          borderRadius: 20 * sizeConv,
         }}
       >
-        <g transform={`translate(${80 * sizeConv},${80 * sizeConv})`}>
-          {arcs.map((arc, i) => (
-            <path
-              key={i}
-              d={
-                d3.arc()({
-                  innerRadius: 35 * sizeConv,
-                  outerRadius: 80 * sizeConv,
-                  startAngle: arc.startAngle,
-                  endAngle: arc.endAngle,
-                  padAngle: 0,
-                })!
-              }
-              fill={tierMapping.get(_stats[i].level)!.detailedColor}
-              stroke={tierMapping.get(_stats[i].level)!.detailedColor}
-            />
-          ))}
-        </g>
-        <circle
-          r={20 * sizeConv}
-          cx={80 * sizeConv}
-          cy={80 * sizeConv}
-          stroke={tierMapping.get(user.tier)!.detailedColor}
-          strokeWidth={8 * sizeConv}
-        />
-        <clipPath id="clip">
-          <circle r={20 * sizeConv} cx={80 * sizeConv} cy={80 * sizeConv} />
-        </clipPath>
-        <image
-          href={
-            (user.profileImageUrl ||
-              'https://static.solved.ac/misc/360x360/default_profile.png') +
-            `?timestamp=${Date.now()}` // prevent image caching in satori. issue: https://github.com/vercel/satori/issues/592
-          }
-          clipPath="url(#clip)"
-          height={40 * sizeConv}
-          width={40 * sizeConv}
-          x={60 * sizeConv}
-          y={60 * sizeConv}
-        />
-      </svg>
+        <svg
+          style={{
+            width: 160 * sizeConv,
+            height: 160 * sizeConv,
+          }}
+        >
+          <g transform={`translate(${85 * sizeConv},${85 * sizeConv})`}>
+            {arcs.map((arc, i) => (
+              <path
+                key={i}
+                d={
+                  d3.arc()({
+                    innerRadius: 35 * sizeConv,
+                    outerRadius: 70 * sizeConv,
+                    startAngle: arc.startAngle,
+                    endAngle: arc.endAngle,
+                    padAngle: 0,
+                  })!
+                }
+                fill={tierMapping.get(_stats[i].level)!.detailedColor}
+                stroke={tierMapping.get(_stats[i].level)!.detailedColor}
+              />
+            ))}
+          </g>
+          <circle
+            r={20 * sizeConv}
+            cx={85 * sizeConv}
+            cy={85 * sizeConv}
+            stroke={tierMapping.get(user.tier)!.detailedColor}
+            strokeWidth={4 * sizeConv}
+          />
+          <clipPath id="clip">
+            <circle r={20 * sizeConv} cx={85 * sizeConv} cy={85 * sizeConv} />
+          </clipPath>
+          <image
+            href={
+              (user.profileImageUrl ||
+                'https://static.solved.ac/misc/360x360/default_profile.png') +
+              `?timestamp=${Date.now()}` // prevent image caching in satori. issue: https://github.com/vercel/satori/issues/592
+            }
+            clipPath="url(#clip)"
+            height={40 * sizeConv}
+            width={40 * sizeConv}
+            x={65 * sizeConv}
+            y={65 * sizeConv}
+          />
+        </svg>
+      </div>
     </div>,
     size,
   )
